3f394eeccab9b78a7f3b88753aefb3636258593b
Pablo Melis <melispablo@Pablos-MacBook-Pro.local>
2024-04-30 15:58:35 +0200

    04/30

---
+++ includes/minishell.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmelis <pmelis@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 22:26:27 by pmelis            #+#    #+#             */
+/*   Updated: 2024/04/29 19:05:51 by pmelis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h> // readline, rl_clear_history, rl_on_new_line,
+								// rl_replace_line, rl_redisplay
+# include <curses.h>            // tgetent, tgetflag, tgetnum, tgetstr, tgoto,
+								//tputs
+# include <dirent.h>            // opendir, readdir, closedir
+# include <errno.h>             // perror
+# include <fcntl.h>             // access, open, read, close
+# include <readline/history.h>  // add_history
+# include <signal.h>            // signal, sigaction, sigemptyset, sigaddset,
+								//kill
+# include <stdio.h>             // printf
+# include <stdlib.h>            // malloc, free, exit, getenv
+# include <string.h>            // strerror
+# include <sys/ioctl.h>         // ioctl
+# include <sys/stat.h>          // stat, lstat, fstat
+# include <sys/types.h>         // fork, wait, waitpid, wait3, wait4
+# include <termios.h>           // isatty, ttyname, ttyslot, tcsetattr,
+								//tcgetattr
+# include <unistd.h>            // write, getcwd, chdir, unlink, execve, dup,
+								//dup2, pipe
+# include "../libft/libft.h"    // libft functions
+
+// Data structures
+typedef enum s_token_type
+{
+	NONE = 0,
+	PIPE = 1,
+	GREATER = 2,
+	GREATER_GREATER = 3,
+	LESS = 4,
+	LESS_LESS = 5,
+}	t_token_type;
+
+typedef struct s_lexer
+{
+	char			*str;
+	t_token_type	token;
+	struct s_lexer	*next;
+	struct s_lexer	*prev;
+}					t_lexer;
+
+// Function prototypes
+
+#endif
--- main.c
+++ main.c
@@ -6,8 +6,43 @@
 /*   By: pmelis <pmelis@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 23:12:58 by pmelis            #+#    #+#             */
-/*   Updated: 2024/01/24 13:02:15 by pmelis           ###   ########.fr       */
+/*   Updated: 2024/04/29 19:07:31 by pmelis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "includes/minishell.h"
+
+int		signal_status = 0;
+
+// Signal handler for SIGINT and SIGQUIT
+void	signal_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		signal_status = 1;
+		write(1, "\nminishell> ", 12);
+	}
+}
+
+t_lexer	*lexer(char *input)
+{
+
+}
+
+
+int	main(void)
+{
+	char	*input;
+
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, signal_handler);
+	while (1)
+	{
+		input = readline("minishell> ");
+		if (!input)
+			break ;
+		add_history(input);
+		free(input);
+	}
+	return (0);
+}
