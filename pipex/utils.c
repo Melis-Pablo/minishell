ed5a1052877c17b9c175b42ecf90cd0028b69212
Pablo Melis <melispablo@Pablos-MacBook-Pro.local>
2024-05-12 15:57:11 +0200

    pipex

---
+++ pipex/utils.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmelis <pmelis@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 16:19:41 by pmelis            #+#    #+#             */
+/*   Updated: 2024/05/09 17:18:02 by pmelis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+/*
+#usage():		Function to display error message when arguments are wrong
+
+#Parameters:
+
+#Return value:
+
+#How it works:
+*/
+void	usage(void)
+{
+	ft_putstr_fd("\033[31mError: Bad argument\n\e[0m", 2);
+	ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <...> <file2>\n", 1);
+	ft_putstr_fd("    ./pipex \"here_doc\" <LIMITER> <cmd> <cmd1> <...> <file>\n", 1);
+	exit(EXIT_SUCCESS);
+}
+
+/*
+#open_file():		Function to open the files with the right flags
+
+#Parameters:
+
+#Return value:
+
+#How it works:
+*/
+int	open_file(char *argv, int i)
+{
+	int	file;
+
+	file = 0;
+	if (i == 0)
+		file = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	else if (i == 1)
+		file = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	else if (i == 2)
+		file = open(argv, O_RDONLY, 0777);
+	if (file == -1)
+		error();
+	return (file);
+}
