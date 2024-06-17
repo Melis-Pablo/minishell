d6936e430a7d72ace9ef5ab91fa83ecb65603cc8
melis-pablo <melispablo@gmail.com>
2024-06-17 14:17:51 +0200

    rands

--- srcs/parser/cmds.c
+++ srcs/parser/cmds.c
@@ -6,7 +6,7 @@
 /*   By: pmelis <pmelis@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 18:43:14 by pmelis            #+#    #+#             */
-/*   Updated: 2024/06/17 12:19:41 by pmelis           ###   ########.fr       */
+/*   Updated: 2024/06/17 14:02:37 by pmelis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,17 @@ char	**get_xtype(t_token *head, t_token_type type)
 	i = 0;
 	tmp = head;
 	type_files = NULL;
+	//count number of files of type
 	while (tmp && tmp->type != PIPE)
 	{
 		if (tmp->type == type)
 			i++;
 		tmp = tmp->next;
 	}
+	// allocate memory for files of type
 	type_files = (char **)malloc(sizeof(char *) * (i + 1));
 	i = 0;
+	// get files of type
 	while (head && head->type != PIPE)
 	{
 		if (head->type == type)
@@ -54,6 +57,67 @@ char	**get_xtype(t_token *head, t_token_type type)
 	}
 	type_files[i] = NULL;
 	return (type_files);
+}
+
+t_redir	*add_redir(t_redir *head, t_redir *new_node)
+{
+	t_redir	*last;
+
+	if (!head)
+		return (new_node);
+	last = head;
+	while (last->next)
+		last = last->next;
+	last->next = new_node;
+	return (head);
+}
+
+t_redir	*get_infiles(t_token *head)
+{
+	t_redir	*infiles;
+	t_token	*tmp;
+	t_redir	*new_node;
+
+	infiles = NULL;
+	tmp = head;
+	new_node = NULL;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == INPUT || tmp->type == HEREDOC)
+		{
+			new_node = (t_redir *)malloc(sizeof(t_redir));
+			new_node->file = ft_strdup(tmp->word);
+			new_node->type = tmp->type;
+			new_node->next = NULL;
+			infiles = add_redir(infiles, new_node);
+		}
+		tmp = tmp->next;
+	}
+	return (infiles);
+}
+
+t_redir *get_outfiles(t_token *head)
+{
+	t_redir	*outfiles;
+	t_token	*tmp;
+	t_redir	*new_node;
+
+	outfiles = NULL;
+	tmp = head;
+	new_node = NULL;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == OUTPUT || tmp->type == APPEND)
+		{
+			new_node = (t_redir *)malloc(sizeof(t_redir));
+			new_node->file = ft_strdup(tmp->word);
+			new_node->type = tmp->type;
+			new_node->next = NULL;
+			outfiles = add_redir(outfiles, new_node);
+		}
+		tmp = tmp->next;
+	}
+	return (outfiles);
 }
 
 char	**get_args(t_token *head)
@@ -161,10 +225,12 @@ t_cmd	*create_node(t_token *head)
 	new_node->cmd = get_cmd(head);
 	new_node->args = get_args(head);
 	new_node->flags = get_flags(head);
-	new_node->infiles = get_xtype(head, INPUT);
-	new_node->outfiles = get_xtype(head, OUTPUT);
-	new_node->heredocs = get_xtype(head, HEREDOC);
-	new_node->appendfiles = get_xtype(head, APPEND);
+	new_node->infiles = get_infiles(head);
+	new_node->outfiles = get_outfiles(head);
+	// new_node->infiles = get_xtype(head, INPUT);
+	// new_node->outfiles = get_xtype(head, OUTPUT);
+	// new_node->heredocs = get_xtype(head, HEREDOC);
+	// new_node->appendfiles = get_xtype(head, APPEND);
 	new_node->prev = NULL;
 	new_node->next = NULL;
 	return (new_node);
--- srcs/redirects/redirects.c
+++ srcs/redirects/redirects.c
@@ -6,89 +6,68 @@
 /*   By: pmelis <pmelis@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 12:28:47 by pmelis            #+#    #+#             */
-/*   Updated: 2024/06/17 13:27:39 by pmelis           ###   ########.fr       */
+/*   Updated: 2024/06/17 14:09:53 by pmelis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-/*
-Input Redirections (<)
+void	redirect_inputs(t_cmd *cmd)
+{
+	t_redir	*tmp;
 
-	Multiple < Redirections:
-		Example: command < infile1 < infile2
-		Behavior: Only the last redirection is used, so the command will read
-			input from infile2.
-		Effect on First Ones: The first redirections (< infile1) have no effect
-			They are ignored.
+	tmp = cmd->infiles;
+	while (tmp)
+	{
+		if (tmp->type == INPUT)
+		{
+			int	fd = open(tmp->file, O_RDONLY);
+			if (fd == -1)
+				perror("input redirection error");
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+		if (tmp->type == HEREDOC)
+		{
+			// Implement here document redirection
+		}
+		tmp = tmp->next;
+	}
+}
 
-Output Redirections (> and >>)
+void	redirect_outputs(t_cmd *cmd)
+{
+	t_redir	*tmp;
 
-	Multiple > Redirections:
-		Example: command > outfile1 > outfile2
-		Behavior: Only the last redirection is used, so the command will write
-			to outfile2, truncating it before writing.
-		Effect on First Ones: The first redirections (> outfile1) have no
-			effect. They are ignored.
+	tmp = cmd->outfiles;
+	while (tmp)
+	{
+		if (tmp->type == OUTPUT)
+		{
+			int	fd = open(tmp->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (fd == -1)
+				perror("output redirection error");
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		if (tmp->type == APPEND)
+		{
+			int	fd = open(tmp->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (fd == -1)
+				perror("append redirection error");
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		tmp = tmp->next;
+	}
+}
 
-	Multiple >> Redirections:
-		Example: command >> appendfile1 >> appendfile2
-		Behavior: Only the last redirection is used, so the command will append
-			to appendfile2.
-		Effect on First Ones: The first redirections (>> appendfile1) have no
-			effect. They are ignored.
+void	close_pipes(int pipes[2])
+{
+	close(pipes[0]);
+	close(pipes[1]);
+}
 
-	Mixing > and >>:
-		Example: command > outfile >> appendfile
-		Behavior: Only the last redirection is used. Here, the command will
-			append to appendfile.
-		Effect on First Ones: The first redirection (> outfile) has no effect.
-			It is ignored.
-
-		Example: command >> appendfile > outfile
-		Behavior: Only the last redirection is used. Here, the command will write
-			to outfile, truncating it before writing.
-		Effect on First Ones: The first redirection (>> appendfile) has no effect
-			It is ignored.
-
-Here Document Redirections (<<)
-
-	Mixing < and <<:
-		< infile is used to redirect input from a file.
-		<< delimiter is used for here documents, where a block of text is
-			provided directly in the command line until the delimiter is reached.
-
-	Example: command < infile << EOF
-		Behavior: The shell processes the input redirection (< infile) first,
-			setting up the input from infile. The here document (<< EOF) is
-			usually processed to provide input directly on the command line,
-			which conflicts with reading from a file.
-		Effect: the last one is used, but everything processed before that is
-			ignored.
-*/
-
-/*
-handle_input_redirections():
-	1. Open the file for reading.
-	2. Redirect the standard input to the file.
-	3. Close the file.
-	4. The command will read input from the file.
-*/
-
-/*
-#find_command():      Searches for the command in the PATH directories.
-
-#Parameters:         [char *] cmd - Command to search for
-
-#Return value:       [char *] - Full path to the command if found, NULL otherwise
-
-#How it works:
-                     1. Gets the PATH environment variable.
-                     2. Splits the PATH into directories.
-                     3. Constructs the full path for each directory.
-                     4. Checks if the command exists and is executable.
-                     5. Returns the full path if found, otherwise NULL.
-*/
 char *find_command(char *cmd)
 {
     char    *path_env;
@@ -99,7 +78,7 @@ char *find_command(char *cmd)
     path_env = getenv("PATH");
     if (!path_env)
         return (NULL);
-    paths = ft_split(path_env, ':');  // Assume ft_split splits a string into an array of strings
+    paths = ft_split(path_env, ':');
     if (!paths)
         return (NULL);
     i = 0;
@@ -123,97 +102,6 @@ char *find_command(char *cmd)
     return (NULL);
 }
 
-/*
-#redirect_input():    Redirects input files to stdin.
-
-#Parameters:         [char **] infiles - Array of input file names
-
-#Return value:       [void] - No return value
-
-#How it works:
-                     1. Iterate over input files.
-                     2. Open each file and redirect its fd to stdin.
-                     3. Close the file descriptor.
-*/
-void	redirect_input(char **infiles)
-{
-	int	fd;
-	int	i;
-
-	i = 0;
-	if (infiles)
-	{
-		while (infiles[i])
-		{
-			fd = open(infiles[i], O_RDONLY);
-			if (fd == -1)
-				perror("input redirection error");
-			dup2(fd, STDIN_FILENO);
-			close(fd);
-			i++;
-		}
-	}
-}
-
-/*
-#redirect_output():   Redirects output and append files to stdout.
-
-#Parameters:         [char **] outfiles - Array of output file names
-                     [char **] appendfiles - Array of append file names
-
-#Return value:       [void] - No return value
-
-#How it works:
-                     1. Iterate over output files.
-                     2. Open each file and redirect its fd to stdout.
-                     3. Close the file descriptor.
-                     4. Repeat for append files with O_APPEND flag.
-*/
-void	redirect_output(char **outfiles, char **appendfiles)
-{
-	int	fd;
-	int	i;
-
-	i = 0;
-	if (outfiles)
-	{
-		while (outfiles[i])
-		{
-			fd = open(outfiles[i], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd == -1)
-				perror("output redirection error");
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-			i++;
-		}
-	}
-	i = 0;
-	if (appendfiles)
-	{
-		while (appendfiles[i])
-		{
-			fd = open(appendfiles[i], O_WRONLY | O_CREAT | O_APPEND, 0644);
-			if (fd == -1)
-				perror("append redirection error");
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-			i++;
-		}
-	}
-}
-
-/*
-#execute_command():   Executes a single command.
-
-#Parameters:         [t_cmd *] cmd - Command structure containing command and arguments
-
-#Return value:       [void] - No return value (exits on failure)
-
-#How it works:
-                     1. Searches for the command in the PATH directories.
-                     2. Executes the command using execve.
-                     3. Prints error message and exits on failure.
-*/
 void	execute_command(t_cmd *cmd)
 {
     char *full_path;
@@ -232,35 +120,6 @@ void	execute_command(t_cmd *cmd)
     free(full_path);
 }
 
-/*
-#close_pipes():       Closes both ends of a pipe.
-
-#Parameters:         [int *] pipes - Array containing pipe file descriptors
-
-#Return value:       [void] - No return value
-
-#How it works:
-                     1. Closes the read and write ends of the pipe.
-*/
-void	close_pipes(int pipes[2])
-{
-	close(pipes[0]);
-	close(pipes[1]);
-}
-
-/*
-#exec_cmds():         Executes a list of commands with pipes.
-
-#Parameters:         [t_cmd *] cmd_list - Linked list of command structures
-
-#Return value:       [int] - Return status
-
-#How it works:
-                     1. Iterates over the command list.
-                     2. Sets up pipes and forks for each command.
-                     3. Redirects input/output as needed.
-                     4. Waits for all child processes to finish.
-*/
 int	exec_cmds(t_cmd *cmd_list)
 {
 	int		pipes[2];
@@ -283,10 +142,6 @@ int	exec_cmds(t_cmd *cmd_list)
 				dup2(pipes[0], STDIN_FILENO);
 			if (cmd_list->next)
 				dup2(pipes[1], STDOUT_FILENO);
-			if (cmd_list->infiles)
-				redirect_input(cmd_list->infiles);
-			if (cmd_list->outfiles || cmd_list->appendfiles)
-				redirect_output(cmd_list->outfiles, cmd_list->appendfiles);
 			execute_command(cmd_list);
 		}
 		if (cmd_list->prev)
